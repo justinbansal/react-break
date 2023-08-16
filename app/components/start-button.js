1be891f7f259561c140ee449a1822176d19e8aa0
Justin <justinbansal@gmail.com>
2023-08-16 16:09:08 -0400

    Notification sent when starting timer using browser api

--- app/components/start-button.js
+++ app/components/start-button.js
@@ -1,12 +1,18 @@
 'use client';
 
 import Timer from '../methods/timer';
+import Notifications from '../methods/notifications';
 
 const timer = new Timer();
+
+const notifications = new Notifications();
 
 export default function StartButton() {
   function handleStartButton() {
     console.log('Start button clicked');
+
+    // Request permission for notifications
+    notifications.requestPermission();
 
     // Start Timer
     timer.start();
